7fc60dc412e45f773d045c878884e92bcdd616db
= <sk8withaguitar@gmail.com>
2017-09-09 15:08:32 -0700

    Basic mounting of other components in react

---
+++ app/javascript/packs/components/Item.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+
+export default class Item extends React.Component {
+  componentWillMount(){
+  }
+  render() {
+    return <h1> Item  </h1>;
+  }
+}
--- app/javascript/packs/components/Product.jsx
+++ app/javascript/packs/components/Product.jsx
@@ -1,3 +1,19 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import Item from './item'
+
+class Product extends React.Component {
+  componentWillMount(){
+  }
+  render() {
+    return <Item />;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Product />,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
